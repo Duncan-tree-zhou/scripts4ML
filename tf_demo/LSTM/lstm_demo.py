9a924953acff6de1a300673bfb29798bc9e07aed
Duncan-tree-zhou <duncan.tree.zhou@gmail.com>
2017-11-12 14:25:47 +0800

    1

---
+++ tf_demo/LSTM/lstm_demo.py
@@ -0,0 +1,11 @@
+"""
+Imports
+"""
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import time
+import os
+import urllib.request
+from tensorflow.models.rnn.ptb import reader
+
